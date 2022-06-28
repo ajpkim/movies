ed24616b1a7b13a0540dd1c89429d81f918889eb
Alex Kim <alex@ajpkim.com>
2022-06-27 22:09:10 -0400

    Setup basic Django project with pick_movie app

--- movies/settings.py
+++ movies/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'pick_movie',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ASGI_APPLICATION = 'movies.asgi.application'
