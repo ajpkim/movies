18fd9c6509484620e202d6e1de94fef6b20257b5
Alex Kim <alex@ajpkim.com>
2022-08-01 15:54:55 -0400

    Convert to class-based API views

--- movie_selection/api/urls.py
+++ movie_selection/api/urls.py
@@ -3,29 +3,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from movie_selection.api import views
 
+# urlpatterns = [
+#     path('rooms/', views.room_list),
+#     path('rooms/<int:pk>/', views.room_detail),
+# ]
+
 urlpatterns = [
-    path('rooms/', views.room_list),
-    path('rooms/<int:pk>/', views.room_detail),
+    path('rooms/', views.RoomList.as_view()),
+    path('rooms/<int:pk>/', views.RoomDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
-
-
-
-# from .views import (
-#     NominationListView,
-#     NominationDetailView,
-#     NominationCreateView,
-#     NominationUpdateView,
-#     NominationDeleteView,
-# )
-
-# app_name = 'movie_selection'
-
-# urlpatterns = [
-    # path('', NominationListView.as_view()),
-    # path('create/', NominationCreateView.as_view()),
-    # path('<pk>', NominationDetailView.as_view()),
-    # path('<pk>/update', NominationUpdateView.as_view()),
-    # path('<pk>/delete/', NominationDeleteView.as_view()),
-# ]
--- movie_selection/api/views.py
+++ movie_selection/api/views.py
@@ -1,42 +1,34 @@
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from movie_selection.models import Nomination, Room, Vote
 from movie_selection.api.serializers import NominationSerializer, RoomSerializer, VoteSerializer
 
-@api_view(['GET', 'POST'])
-def room_list(request, format=None):
-    """
-    List all rooms, or create a new room.
-    """
-    if request.method == 'GET':
+class RoomList(APIView):
+    def get(self, request, format=None):
         rooms = Room.objects.all()
         serializer = RoomSerializer(rooms, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, format=None):
         serializer = RoomSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-def room_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a room.
-    """
-    try:
-        room = Room.objects.get(pk=pk)
-    except Room.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class RoomDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(id=pk)
+        except Room.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
-        data = {'room_name': room.name, 'nominations': room.nominations_data}
+    def get(self, request, pk, format=None):
+        room = self.get_object(pk)
+        room_nominations = room.nominations_data
+        data = {'room_name': room.name, 'nominations': room_nominations}
         return Response(data)
-
-    elif request.method == 'POST':
-        serializer = RoomSerializer(room, data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
